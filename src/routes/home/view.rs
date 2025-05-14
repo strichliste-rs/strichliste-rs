697321c512284f08e787c92a059db03c7c5c3263
DestinyofYeet <olebend@gmail.com>
2025-05-14 19:02:03 +0200

    feat(routes/homepage): Started split up

--- src/routes/home/view.rs
+++ src/routes/home/view.rs
@@ -3,8 +3,18 @@ use leptos::prelude::*;
 #[component]
 pub fn View() -> impl IntoView {
     view! {
-        <div class="bg-sky-50 flex justify-center">
-            <h1>"Homepage"</h1>
+        <div class="grid grid-cols-10 gap-10">
+            <div class="bg-blue-900 col-span-1">
+                // button
+                <p>"Button"</p>
+            </div>
+            <div class="flext justify-start col-span-9">
+                // content
+                <p>"Content"</p>
+            </div>
         </div>
     }
 }
+
+#[component]
+pub fn ShowUsers() -> impl IntoView {}
