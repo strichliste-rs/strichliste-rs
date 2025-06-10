be6fb66ff4a4bf38560dc2e2932f5b041d255213
DestinyofYeet <ole@ole.blue>
2025-06-11 00:00:48 +0200

    feat(routes/article/create): Adds styling to table

--- src/routes/articles/show.rs
+++ src/routes/articles/show.rs
@@ -3,6 +3,24 @@ use tracing::error;
 
 use crate::models::{Article, User};
 
+#[server]
+pub async fn get_all_articles() -> Result<Vec<Article>, ServerFnError> {
+    use crate::backend::ServerState;
+    let state: ServerState = expect_context();
+    use axum::http::StatusCode;
+    use leptos_axum::ResponseOptions;
+
+    let response_opts: ResponseOptions = expect_context();
+
+    let articles = Article::get_all_from_db(&*state.db.lock().await).await;
+    articles.map_err(|e| {
+        let err = e.to_string();
+        error!("Could not fetch articles {}", err);
+        response_opts.set_status(StatusCode::INTERNAL_SERVER_ERROR);
+        ServerFnError::new(err)
+    })
+}
+
 #[component]
 pub fn View() -> impl IntoView {
     view! {
@@ -43,22 +61,29 @@ fn ShowArticles() -> impl IntoView {
                             }.into_any()
                         },
                         Ok(articles) => view!{
-                            <table>
-                            <tr>
-                              <th>"Name"</th>
-                              <th>"Preis"</th>
-                            </tr>
-                            {
-                                articles.into_iter().map(|article| {
-                                    view!{
-                                        <tr>
-                                          <td>{article.name}</td>
-                                          <td>{article.cost.format_eur()}</td>
-                                        </tr>
-                                    }
-                                }).collect_view()
+                            <table class="w-full text-white p-2">
+                                <thead>
+                                    <tr class="bg-black">
+                                      <th>"Name"</th>
+                                      <th>"Preis"</th>
+                                      <th></th>                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        articles.into_iter().map(|article| {
+                                            view!{
+                                                <tr class="even:bg-gray-700 odd:bg-gray-500">
+                                                  <td class="p-2 text-center">{article.name}</td>
+                                                  <td class="p-2 text-center">{article.cost.format_eur()}</td>
+                                                  <td class="bg-green-700 p-2">
+                                                      <a href=format!("/articles/{}", article.id.unwrap())><p class="text-center">"Edit"</p></a>
+                                                  </td>
+                                                </tr>
+                                            }
+                                        }).collect_view()
 
-                            }
+                                    }
+                                </tbody>
                             </table>
                         }.into_any(),
                     }
@@ -66,22 +91,4 @@ fn ShowArticles() -> impl IntoView {
                 }
             </Suspense>
     }
-}
-
-#[server]
-pub async fn get_all_articles() -> Result<Vec<Article>, ServerFnError> {
-    use crate::backend::ServerState;
-    let state: ServerState = expect_context();
-    use axum::http::StatusCode;
-    use leptos_axum::ResponseOptions;
-
-    let response_opts: ResponseOptions = expect_context();
-
-    let articles = Article::get_all_from_db(&*state.db.lock().await).await;
-    articles.map_err(|e| {
-        let err = e.to_string();
-        error!("Could not fetch articles {}", err);
-        response_opts.set_status(StatusCode::INTERNAL_SERVER_ERROR);
-        ServerFnError::new(err)
-    })
 }
