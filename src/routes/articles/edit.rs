d9874628ab62effb55087ccb6eac63d46496d56b
DestinyofYeet <ole@ole.blue>
2025-06-21 00:59:55 +0200

    feat(routes/user/extra_transactions): Only updates value if it's different

--- src/routes/articles/edit.rs
+++ src/routes/articles/edit.rs
@@ -5,9 +5,9 @@ use leptos::{
     task::spawn_local,
 };
 use leptos_router::hooks::use_params_map;
-use tracing::error;
+use tracing::{debug, error};
 
-use crate::models::{Article, Barcode, BarcodeDiff};
+use crate::models::{Article, Barcode, BarcodeDiff, Money};
 
 #[server]
 pub async fn get_article(article_id: i64) -> Result<Article, ServerFnError> {
@@ -56,7 +56,7 @@ pub async fn update_article(id: i64, name: String, cost: String, barcodes: Optio
 
     let mut article = get_article(id).await?;
 
-    let cost = match cost.clone().try_into() {
+    let cost: Money = match cost.clone().try_into() {
       Ok(value) => value,
       Err(e) => {
           response_opts.set_status(StatusCode::BAD_REQUEST);
@@ -66,7 +66,7 @@ pub async fn update_article(id: i64, name: String, cost: String, barcodes: Optio
 
     let db = &*state.db.lock().await;
 
-    let mut transaction = match db.get_conn_transaction().await {
+    let mut db_transaction = match db.get_conn_transaction().await {
         Ok(value) => value,
         Err(e) => {
             response_opts.set_status(StatusCode::INTERNAL_SERVER_ERROR);
@@ -75,28 +75,36 @@ pub async fn update_article(id: i64, name: String, cost: String, barcodes: Optio
         }
     };
 
-    match article.set_name(&mut *transaction, name.trim().to_string()).await {
-        Ok(_) => {},
-        Err(e) => {
-          response_opts.set_status(StatusCode::INTERNAL_SERVER_ERROR);
-          error!("Failed to update article name: {}", e);
-          return Err(ServerFnError::new(format!("Failed to update article name: {}", e)));
-        },
-    }
-
-    match article.set_cost(&mut *transaction, cost).await {
-        Ok(_) => {},
-        Err(e) => {
-            response_opts.set_status(StatusCode::INTERNAL_SERVER_ERROR);
-            error!("Failed to update article cost: {}", e);
-            return Err(ServerFnError::new("FAiled to update article cost"));
+    if article.name != name {
+        
+        match article.set_name(&mut *db_transaction, name.trim().to_string()).await {
+            Ok(_) => {},
+            Err(e) => {
+              response_opts.set_status(StatusCode::INTERNAL_SERVER_ERROR);
+              error!("Failed to update article name: {}", e);
+              return Err(ServerFnError::new(format!("Failed to update article name: {}", e)));
+            },
         }
     }
+
+    debug!("Old money: {} | New money: {}", article.cost.value, cost.value);
+
+    if article.cost.value != cost.value {
+        match article.set_cost(&mut *db_transaction, cost).await {
+            Ok(_) => {},
+            Err(e) => {
+                response_opts.set_status(StatusCode::INTERNAL_SERVER_ERROR);
+                error!("Failed to update article cost: {}", e);
+                return Err(ServerFnError::new("FAiled to update article cost"));
+            }
+        }
+    }
+
 
     match barcodes {
         None => {},
         Some(barcodes) => {
-            let result = article.set_barcodes(&mut *transaction, barcodes).await;
+            let result = article.set_barcodes(&mut *db_transaction, barcodes).await;
 
             match result {
                 Err(e) => {
@@ -110,7 +118,7 @@ pub async fn update_article(id: i64, name: String, cost: String, barcodes: Optio
         },
     }
 
-    match transaction.commit().await {
+    match db_transaction.commit().await {
         Ok(_) => {},
         Err(e) => {
             response_opts.set_status(StatusCode::INTERNAL_SERVER_ERROR);
