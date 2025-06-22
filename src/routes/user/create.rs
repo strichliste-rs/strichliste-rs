7aaba2cdbcb9b644cf64b34dc67c88ca9c2e0bc6
DestinyofYeet <ole@ole.blue>
2025-06-23 00:40:36 +0200

    feat(routes/user/create): Disables autocomplete on input form

--- src/routes/user/create.rs
+++ src/routes/user/create.rs
@@ -44,7 +44,7 @@ pub fn Create() -> impl IntoView {
               <ActionForm action=create_user_action>
                 <div>
                   <label class="block mb-2 text-indigo-500" for="username">Nickname</label>
-                  <input class="w-full p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300" type="text" name="username"/>
+                  <input autocomplete="off" class="w-full p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300" type="text" name="username"/>
                 </div>
                 <div>
                   <input class="w-full bg-indigo-700 hover:bg-pink-700 text-white font-bold py-2 px-4 mb-6 rounded" type="submit" value="Create Account"/>
