097cbd32def1bb561f40773706c512508c7a811c
DestinyofYeet <ole@ole.blue>
2025-09-12 23:04:17 +0200

    chore(inactive_users): Removed dead link for inactive users

--- src/routes/navbar.rs
+++ src/routes/navbar.rs
@@ -7,7 +7,6 @@ pub fn View() -> impl IntoView {
         <nav>
             <div class="flex flex-row gap-4 p-8 bg-[#1d2832] text-gray-200">
                 <a href="/">"Strichliste"</a>
-                <a href="/inactive_users">"Inactive users"</a>
                 <a href="/articles">"Article list"</a>
             </div>
         </nav>
