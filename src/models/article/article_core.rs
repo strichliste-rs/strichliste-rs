d4ff8dbf2ee5be4ea6980153c1c72834d9a8331a
haennes <hannes.hofmuth@gmail.com>
2025-09-19 00:14:03 +0200

    todo(move constant) add todo

--- src/models/article/article_core.rs
+++ src/models/article/article_core.rs
@@ -28,6 +28,7 @@ pub struct Article {
 }
 
 impl Article {
+    //TODO move this to the relevant component directly or to a constants file in frontend
     pub const DEFAULT_ARTICLE_AMOUNT: usize = 9;
 }
 
