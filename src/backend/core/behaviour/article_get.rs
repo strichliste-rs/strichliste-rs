b0c129258c74a4bdd9baaaa4f8d1d86df4091cf4
haennes <hannes.hofmuth@gmail.com>
2025-09-19 14:39:49 +0200

    mv(Article::get) mot to behaviours

---
+++ src/backend/core/behaviour/article_get.rs
@@ -0,0 +1,37 @@
+#![cfg(feature = "ssr")]
+
+use crate::{
+    backend::{
+        core::Article,
+        database::{DatabaseResponse, DB},
+    },
+    models::{ArticleDB, Barcode},
+};
+impl Article {
+    pub async fn get(db: &DB, id: i64) -> DatabaseResponse<Option<Self>> {
+        let mut conn = db.get_conn().await?;
+
+        match ArticleDB::get_single(&mut *conn, id).await? {
+            Some(article) => {
+                let article_sounds = ArticleDB::get_sounds(&mut *conn, article.id).await?;
+                let article_barcodes = ArticleDB::get_barcodes(&mut *conn, article.id)
+                    .await?
+                    .into_iter()
+                    .map(|elem| Barcode(elem.barcode_content))
+                    .collect();
+
+                let cost = ArticleDB::get_latest_cost(&mut *conn, article.id).await?;
+
+                let ArticleDB { id, name } = article;
+                Ok(Some(Article {
+                    id,
+                    name,
+                    cost: cost.into(),
+                    sounds: article_sounds,
+                    barcodes: article_barcodes,
+                }))
+            }
+            None => Ok(None),
+        }
+    }
+}
--- src/backend/core/behaviour/mod.rs
+++ src/backend/core/behaviour/mod.rs
@@ -1,2 +1,3 @@
+pub mod article_get;
 pub mod article_get_all;
 pub mod article_new;
