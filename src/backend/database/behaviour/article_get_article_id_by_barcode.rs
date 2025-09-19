f8bbe6b800291e1052059ba363b242e861eb7962
haennes <hannes.hofmuth@gmail.com>
2025-09-19 17:30:29 +0200

    mv(ArticleDB::get_article_id_by_barcode) move to behaviour

---
+++ src/backend/database/behaviour/article_get_article_id_by_barcode.rs
@@ -0,0 +1,32 @@
+#![cfg(feature = "ssr")]
+
+use sqlx::{query, Executor};
+
+use crate::{
+    backend::database::{ArticleDB, DBError, DatabaseResponse, DatabaseType},
+    models::DatabaseId,
+};
+
+impl ArticleDB {
+    pub async fn get_article_id_by_barcode<T>(
+        conn: &mut T,
+        barcode: String,
+    ) -> DatabaseResponse<Option<DatabaseId>>
+    where
+        for<'a> &'a mut T: Executor<'a, Database = DatabaseType>,
+    {
+        let result = query!(
+            "
+                select article_id from ArticleBarcodes
+                where barcode_content = ?
+            ",
+            barcode
+        )
+        .fetch_optional(&mut *conn)
+        .await
+        .map_err(DBError::new)?
+        .map(|elem| elem.article_id);
+
+        Ok(result)
+    }
+}
--- src/models/article/article_core.rs
+++ src/models/article/article_core.rs
@@ -6,7 +6,7 @@ use crate::backend::database::ArticleDB;
 use {
     crate::backend::database::DBError,
     crate::backend::database::{DatabaseResponse, DatabaseType},
-    crate::models::{DatabaseId, UserId},
+    crate::models::UserId,
     sqlx::query,
     sqlx::Executor,
 };
@@ -18,28 +18,6 @@ impl Article {
 
 #[cfg(feature = "ssr")]
 impl ArticleDB {
-    pub async fn get_article_id_by_barcode<T>(
-        conn: &mut T,
-        barcode: String,
-    ) -> DatabaseResponse<Option<DatabaseId>>
-    where
-        for<'a> &'a mut T: Executor<'a, Database = DatabaseType>,
-    {
-        let result = query!(
-            "
-                select article_id from ArticleBarcodes
-                where barcode_content = ?
-            ",
-            barcode
-        )
-        .fetch_optional(&mut *conn)
-        .await
-        .map_err(DBError::new)?
-        .map(|elem| elem.article_id);
-
-        Ok(result)
-    }
-
     /// returns the article_id and amount of items bought for the user
     pub async fn get_articles_for_user<T>(
         conn: &mut T,
