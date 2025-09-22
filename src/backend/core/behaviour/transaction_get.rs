d04bf8190e43304fafbd81ec30085657098259b7
haennes <hannes.hofmuth@gmail.com>
2025-09-22 23:13:50 +0200

    mv(TransactionDB::get) move to behaviour

---
+++ src/backend/core/behaviour/transaction_get.rs
@@ -0,0 +1,36 @@
+#![cfg(feature = "ssr")]
+
+use sqlx::Executor;
+
+use crate::{
+    backend::database::{DBError, DatabaseResponse, DatabaseType, TransactionDB},
+    model::UserId,
+    models::{DatabaseId, Transaction},
+};
+
+impl Transaction {
+    pub async fn get<T>(
+        conn: &mut T,
+        id: DatabaseId,
+        user_id: UserId,
+    ) -> DatabaseResponse<Option<Self>>
+    where
+        for<'a> &'a mut T: Executor<'a, Database = DatabaseType>,
+    {
+        use crate::backend::database::GroupDB;
+
+        let transaction_db = TransactionDB::get(conn, id).await?;
+
+        let transaction_db = match transaction_db {
+            Some(value) => value,
+            None => return Ok(None),
+        };
+
+        let user_groups = GroupDB::get_groups(&mut *conn, user_id).await?;
+
+        let transaction: Transaction = (transaction_db, &user_groups)
+            .try_into()
+            .map_err(DBError::new)?;
+        Ok(Some(transaction))
+    }
+}
--- src/models/transaction.rs
+++ src/models/transaction.rs
@@ -335,31 +335,6 @@ pub struct Transaction {
 
 #[cfg(feature = "ssr")]
 impl Transaction {
-    pub async fn get<T>(
-        conn: &mut T,
-        id: DatabaseId,
-        user_id: UserId,
-    ) -> DatabaseResponse<Option<Self>>
-    where
-        for<'a> &'a mut T: Executor<'a, Database = DatabaseType>,
-    {
-        use crate::backend::database::GroupDB;
-
-        let transaction_db = TransactionDB::get(conn, id).await?;
-
-        let transaction_db = match transaction_db {
-            Some(value) => value,
-            None => return Ok(None),
-        };
-
-        let user_groups = GroupDB::get_groups(&mut *conn, user_id).await?;
-
-        let transaction: Transaction = (transaction_db, &user_groups)
-            .try_into()
-            .map_err(DBError::new)?;
-        Ok(Some(transaction))
-    }
-
     pub async fn get_user_transactions(
         db: &DB,
         user_id: UserId,
