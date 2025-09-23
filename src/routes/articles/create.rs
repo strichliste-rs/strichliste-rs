0922dec374cd23bc309dcc2d261331ab2d6af0fb
haennes <hannes.hofmuth@gmail.com>
2025-09-23 13:29:28 +0200

    mv(create_article serverfn) move to behaviour

--- src/routes/articles/create.rs
+++ src/routes/articles/create.rs
@@ -1,53 +1,6 @@
 use leptos::prelude::*;
 
-#[cfg(feature = "ssr")]
-use crate::{backend::core::Article, model::Money};
-
-#[server]
-pub async fn create_article(name: String, cost: String) -> Result<(), ServerFnError> {
-    use crate::backend::core::ServerState;
-    use axum::http::StatusCode;
-    use leptos_axum::redirect;
-    use leptos_axum::ResponseOptions;
-    use tracing::{debug, error};
-    let state: ServerState = expect_context();
-
-    let response_opts: ResponseOptions = expect_context();
-    debug!("Creating article!");
-
-    if name.is_empty() {
-        response_opts.set_status(StatusCode::BAD_REQUEST);
-        return Err(ServerFnError::new("Name cannot be empty!"));
-    }
-
-    if cost.is_empty() {
-        response_opts.set_status(StatusCode::BAD_REQUEST);
-        return Err(ServerFnError::new("Cost cannot be empty!"));
-    }
-
-    let money: Money = match cost.try_into() {
-        Ok(value) => value,
-        Err(err) => {
-            response_opts.set_status(StatusCode::BAD_REQUEST);
-            return Err(ServerFnError::new(err));
-        }
-    };
-
-    let article = Article::new(&*state.db.lock().await, name, money).await;
-
-    let article = match article {
-        Ok(value) => value,
-        Err(e) => {
-            response_opts.set_status(StatusCode::INTERNAL_SERVER_ERROR);
-            error!("Failed to create new article: {}", e);
-            return Err(ServerFnError::new("Failed to create article!"));
-        }
-    };
-
-    redirect(&format!("/articles/{}", article.id));
-
-    Ok(())
-}
+use crate::backend::core::behaviour::article_new::CreateArticle;
 
 #[component]
 pub fn Create() -> impl IntoView {
