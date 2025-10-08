b089e7aef53b2ba3eeacb5483cc312a66251ee5a
DestinyofYeet <ole@ole.blue>
2025-10-08 02:45:20 +0200

    Added popover when creating a new user

--- src/frontend/route/home.rs
+++ src/frontend/route/home.rs
@@ -1,22 +1,79 @@
-use leptos::prelude::*;
+use leptos::{ev, prelude::*};
+use thaw::{
+    Button, ButtonType, Field, FieldContextInjection, FieldContextProvider, Flex, FlexGap, Input,
+    InputRule, Popover, PopoverTrigger, PopoverTriggerType,
+};
 
-use crate::frontend::component::{invisible_scan_input::ScanUserBarcodeListener, user::ShowUsers};
+use crate::{
+    backend::core::behaviour::user_create::CreateUser,
+    frontend::{
+        component::{invisible_scan_input::ScanUserBarcodeListener, user::ShowUsers},
+        shared::throw_error,
+    },
+};
 
 #[component]
 pub fn View() -> impl IntoView {
+    let create_user_action = ServerAction::<CreateUser>::new();
+
+    Effect::new(move || {
+        if let Some(Err(e)) = create_user_action.value().get() {
+            let msg = match e {
+                ServerFnError::ServerError(msg) => msg,
+                _ => e.to_string(),
+            };
+            throw_error(format!("Failed to add user: {msg}"));
+        }
+    });
+
+    let ignore_scan_input_signal = RwSignal::new(false);
     view! {
         <div class="grid grid-cols-10 gap-10 py-10 h-screen">
             <div class="col-span-1 pl-5 justify-self-center">
-                <a href="/user/create" class="inline-block">
-                    <div class="flex justify-center">
-                        // joinked from: https://gist.github.com/ibelick/0c92c1aba54c2f7e8b3a7381426ed029
-                        <button class="inline-flex h-10 w-10 items-center justify-center rounded-full bg-gray-50 text-black drop-shadow-sm transition-colors duration-150 hover:bg-gray-200">
-                            "+"
-                        </button>
-                    </div>
-                </a>
+                <div class="flex justify-center">
+                    // joinked from: https://gist.github.com/ibelick/0c92c1aba54c2f7e8b3a7381426ed029
+                    <Popover
+                        trigger_type=PopoverTriggerType::Click
+                        on_open=move || { ignore_scan_input_signal.set(true) }
+
+                        on_close=move || { ignore_scan_input_signal.set(false) }
+                    >
+                        <PopoverTrigger slot>
+                            <button class="inline-flex h-10 w-10 items-center justify-center rounded-full bg-gray-50 text-black drop-shadow-sm transition-colors duration-150 hover:bg-gray-200">
+                                "+"
+                            </button>
+                        </PopoverTrigger>
+                        <form autocomplete="off">
+                            <ActionForm action=create_user_action>
+                                <FieldContextProvider>
+                                    <Flex gap=FlexGap::Medium>
+                                        <Field name="username">
+                                            <Input
+                                                rules=vec![InputRule::required(true.into())]
+                                                autofocus=true
+                                            />
+                                        </Field>
+                                        <Button
+                                            button_type=ButtonType::Submit
+                                            on_click={
+                                                let field_context = FieldContextInjection::expect_context();
+                                                move |e: ev::MouseEvent| {
+                                                    if !field_context.validate() {
+                                                        e.prevent_default()
+                                                    }
+                                                }
+                                            }
+                                        >
+                                            "Create User"
+                                        </Button>
+                                    </Flex>
+                                </FieldContextProvider>
+                            </ActionForm>
+                        </form>
+                    </Popover>
+                </div>
             </div>
-            {ScanUserBarcodeListener()}
+            <ScanUserBarcodeListener ignore_input=ignore_scan_input_signal />
             <div class="col-span-9 pr-7">{ShowUsers()}</div>
         </div>
     }
--- src/frontend/route/user/send_money.rs
+++ src/frontend/route/user/send_money.rs
@@ -1,19 +1,23 @@
-use leptos::{html, leptos_dom::logging::console_log, prelude::*, task::spawn_local};
+use leptos::{ev, prelude::*};
 use leptos_router::hooks::use_params_map;
-use thaw::{Toast, ToastBody, ToastTitle, ToasterInjection};
+use thaw::{
+    AutoComplete, AutoCompleteOption, Button, ButtonType, Field, FieldContextInjection,
+    FieldContextProvider, Flex, FlexAlign, FlexGap, FlexJustify, Input, Label,
+};
 
 use crate::{
     backend::core::behaviour::{
-        send_money::send_money, user_get::get_user, user_get_all::get_all_users,
+        send_money::SendMoney, user_get::get_user, user_get_all::get_all_users,
     },
-    frontend::shared::throw_error_none_view,
+    frontend::shared::{throw_error, throw_error_none_view},
     model::UserId,
 };
+
+const USER_SEARCH_LIMIT: usize = 10;
 
 #[component]
 pub fn Show() -> impl IntoView {
     let params = use_params_map();
-    let toaster = ToasterInjection::expect_context();
     let user_id_string = params.read_untracked().get("id").unwrap_or_default();
 
     let user_id = match user_id_string.parse::<i64>() {
@@ -30,11 +34,8 @@ pub fn Show() -> impl IntoView {
     let user_resource = OnceResource::new(get_user(user_id));
     let all_users_resource = OnceResource::new(get_all_users());
 
-    let receiver_input = RwSignal::new(String::new());
-    let amount_input = RwSignal::new(String::new());
-    let hidden_div_node_ref = NodeRef::<html::Div>::new();
+    let send_money_action = ServerAction::<SendMoney>::new();
 
-    let error_result = RwSignal::new(String::new());
     view! {
         <Suspense fallback=move || {
             view! { <p class="text-white text-center">"Loading user"</p> }
@@ -58,128 +59,92 @@ pub fn Show() -> impl IntoView {
                             }
                         };
                         let all_users = match all_users_resource.get() {
-                            Some(Ok(value)) => value,
+                            Some(Ok(value)) => RwSignal::new(value),
                             _ => {
                                 return ().into_any();
                             }
                         };
+                        let selected_user_input = RwSignal::new(String::new());
+                        let money_amount_input = RwSignal::new(String::new());
+                        let options = Memo::<
+                            Vec<String>,
+                        >::new(move |_| {
+                            all_users
+                                .get()
+                                .iter()
+                                .filter(|elem| elem.id != user.id)
+                                .filter(|elem| {
+                                    elem
+                                        .nickname
+                                        .to_lowercase()
+                                        .contains(&selected_user_input.get().to_lowercase())
+                                })
+                                .map(|elem| elem.nickname.clone())
+                                .take(USER_SEARCH_LIMIT)
+                                .collect()
+                        });
+                        Effect::new(move || {
+                            if let Some(Err(e)) = send_money_action.value().get() {
+                                throw_error(format!("Failed to send money: {e}"));
+                            }
+                        });
 
                         view! {
-                            <div class="flex h-screen bg-gray-900">
-                                <div class="w-full max-w-xs m-auto bg-indigo-100 rounded p-5">
-                                    <p class="text-indigo-500 text-center">
+                            <div class="pt-5">
+                                <Flex
+                                    justify=FlexJustify::Center
+                                    align=FlexAlign::Center
+                                    vertical=true
+                                >
+                                    <Label>
                                         "Hello "{user.nickname}", who do you want to send money to?"
-                                    </p>
-                                    <div>
-                                        <label class="block mb-2 text-indigo-500">"Receiver"</label>
-                                        <input
-                                            bind:value=receiver_input
-                                            autocomplete="off"
-                                            class="w-full p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300"
-                                            type="text"
-                                            name="to_user"
-                                        />
-                                    </div>
-                                    <div
-                                        class="flex flex-col items-center"
-                                        class=("hidden", move || receiver_input.get().is_empty())
-                                        node_ref=hidden_div_node_ref
-                                    >
-                                        {move || {
-                                            let search = receiver_input.get();
-                                            all_users
-                                                .iter()
-                                                .filter(|elem| elem.id != user.id)
-                                                .filter(|elem| {
-                                                    elem.nickname
-                                                        .to_lowercase()
-                                                        .contains(&search.to_lowercase())
-                                                })
-                                                .map(|elem| {
-                                                    let nickname = elem.nickname.clone();
-                                                    let n_clone = nickname.clone();
-
-                                                    view! {
-                                                        <button
-                                                            class="bg-gray-400 text-white p-2 rounded"
-                                                            on:click=move |_| {
-                                                                receiver_input.set(n_clone.clone());
-                                                                hidden_div_node_ref
-                                                                    .get()
-                                                                    .map(|elem| {
-                                                                        elem.class("hidden flex flex-col items-center")
-                                                                    });
-                                                            }
+                                    </Label>
+                                    <ActionForm action=send_money_action prop:autocomplete="off">
+                                        <FieldContextProvider>
+                                            <Flex
+                                                vertical=true
+                                                align=FlexAlign::Center
+                                                gap=FlexGap::Medium
+                                            >
+                                                <Field name="to_user" label="Receiver" required=true>
+                                                    <AutoComplete
+                                                        value=selected_user_input
+                                                        placeholder="Receiving User"
+                                                    >
+                                                        <For
+                                                            each=move || options.get()
+                                                            key=|option| option.clone()
+                                                            let:option
                                                         >
-                                                            {nickname}
-                                                        </button>
-                                                    }
-                                                })
-                                                .collect_view()
-                                        }}
-                                    </div>
-                                    <div>
-                                        <label class="block mb-2 text-indigo-500">"Amount"</label>
-                                        <input
-                                            bind:value=amount_input
-                                            autocomplete="off"
-                                            class="w-full p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300"
-                                            type="text"
-                                            name="amount"
-                                        />
-                                    </div>
-                                    <div>
-                                        <input
-                                            class="w-full bg-indigo-700 hover:bg-pink-700 text-white font-bold py-2 px-4 mb-6 rounded"
-                                            type="submit"
-                                            value="Send money"
-                                            on:click=move |_| {
-                                                spawn_local(async move {
-                                                    console_log("Need to send_money ()");
-                                                    match send_money(
-                                                            user_id,
-                                                            receiver_input.get_untracked(),
-                                                            amount_input.get_untracked(),
-                                                        )
-                                                        .await
-                                                    {
-                                                        Ok(_) => {
-                                                            toaster
-                                                                .dispatch_toast(
-                                                                    move || {
-                                                                        view! {
-                                                                            <Toast>
-                                                                                <ToastTitle>"Money sent"</ToastTitle>
-                                                                                <ToastBody>
-                                                                                    "You sent "{amount_input.get_untracked()}" to "
-                                                                                    {receiver_input.get_untracked()}
-                                                                                </ToastBody>
-                                                                            </Toast>
-                                                                        }
-                                                                    },
-                                                                    Default::default(),
-                                                                );
-                                                        }
-                                                        Err(e) => {
-                                                            error_result.set(e.to_string());
+                                                            <AutoCompleteOption value=option
+                                                                .clone()>{option}</AutoCompleteOption>
+                                                        </For>
+                                                    </AutoComplete>
+                                                </Field>
+                                                <Field name="amount" label="Amount" required=true>
+                                                    <Input value=money_amount_input />
+                                                </Field>
+
+                                                <input type="hidden" name="user_id" value=user.id.0 />
+
+                                                <Button
+                                                    button_type=ButtonType::Submit
+                                                    on_click={
+                                                        let field_context = FieldContextInjection::expect_context();
+                                                        move |e: ev::MouseEvent| {
+                                                            if !field_context.validate() {
+                                                                e.prevent_default()
+                                                            }
                                                         }
                                                     }
-                                                });
-                                            }
-                                        />
-                                    </div>
-                                    <div>
-                                        {move || match error_result.get().len() {
-                                            0 => ().into_any(),
-                                            _ => {
-                                                let msg = error_result.get();
-                                                throw_error_none_view(
-                                                    format!("Failed to send money:  {msg}"),
-                                                )
-                                            }
-                                        }}
-                                    </div>
-                                </div>
+                                                >
+                                                    "Send money"
+                                                </Button>
+                                            </Flex>
+                                        </FieldContextProvider>
+                                    </ActionForm>
+                                </Flex>
+
                             </div>
                         }
                             .into_any()
