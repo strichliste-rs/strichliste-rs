03cfd4dc05f69c7053c742f0815aeabe30ac7675
DestinyofYeet <olebend@gmail.com>
2025-10-13 15:45:15 +0200

    Converted Split Transaction

--- src/frontend/component/single_user_selection.rs
+++ src/frontend/component/single_user_selection.rs
@@ -1,4 +1,5 @@
-use leptos::{html, prelude::*};
+use leptos::prelude::*;
+use thaw::{AutoComplete, AutoCompleteOption, Field};
 
 use crate::{
     backend::core::behaviour::user_get_all::get_all_users,
@@ -12,12 +13,11 @@ pub fn SelectSingleUser(
     title: String,
     input: RwSignal<String>,
     #[prop(optional)] children: Option<ChildrenFn>,
-    #[prop(optional)] extra_class: Option<String>,
     #[prop(optional)] filter_names: Option<RwSignal<Vec<String>>>,
 ) -> impl IntoView {
-    let hidden_div_node_ref = NodeRef::<html::Div>::new();
-
     let all_users_resource = OnceResource::new(get_all_users());
+
+    let title = RwSignal::new(title);
 
     view! {
         <Suspense fallback=move || {
@@ -25,7 +25,7 @@ pub fn SelectSingleUser(
         }>
             {move || {
                 let all_users = match all_users_resource.get() {
-                    Some(Ok(value)) => value,
+                    Some(Ok(value)) => RwSignal::new(value),
                     _ => {
                         return view! {
                             <ErrorMessage error=String::from("Failed to fetch users!") />
@@ -34,77 +34,54 @@ pub fn SelectSingleUser(
                     }
                 };
 
-                view! {
-                    <div class=format!("flex {}", extra_class.as_ref().map_or("", |v| v))>
-                        <div class=format!(
-                            "bg-indigo-100 rounded p-5 {}",
-                            extra_class.as_ref().map_or("", |v| v),
-                        )>
-                            <div>
-                                <label class="block mb-2 text-indigo-500">{title.clone()}</label>
-                                <input
-                                    bind:value=input
-                                    autocomplete="off"
-                                    class="w-full p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300"
-                                    type="text"
-                                    name="to_user"
-                                />
-                            </div>
-                            <div
-                                class="flex flex-col items-center gap-2"
-                                class=("hidden", move || input.get().is_empty())
-                                node_ref=hidden_div_node_ref
-                            >
-                                {move || {
-                                    let search = input.get();
-                                    all_users
-                                        .iter()
-                                        .filter(|elem| {
-                                            elem.nickname
-                                                .to_lowercase()
-                                                .contains(&search.to_lowercase())
-                                        })
-                                        .filter(|elem| {
-                                            match filter_names {
-                                                None => true,
-                                                Some(signal) => {
-                                                    let value = signal.get();
-                                                    for name in value.iter() {
-                                                        if *name == *elem.nickname {
-                                                            return false;
-                                                        }
-                                                    }
-                                                    true
-                                                }
-                                            }
-                                        })
-                                        .take(MAX_USER_DISPLAY_LIMIT)
-                                        .map(|elem| {
-                                            let nickname = elem.nickname.clone();
-                                            let n_clone = nickname.clone();
+                let options = Memo::<
+                    Vec<String>,
+                >::new(move |_| {
+                    all_users
+                        .get()
+                        .iter()
+                        .filter(|elem| {
+                            elem.nickname
+                                .to_lowercase()
+                                .contains(&input.get().to_lowercase())
+                        })
+                        .filter(|elem| {
+                            match filter_names {
+                                Some(names) => {
+                                    for name in names.get().iter() {
+                                        if name == &elem.nickname {
+                                            return false;
+                                        }
+                                    }
 
-                                            view! {
-                                                <button
-                                                    class="bg-gray-400 text-white p-2 rounded"
-                                                    on:click=move |_| {
-                                                        input.set(n_clone.clone());
-                                                        hidden_div_node_ref
-                                                            .get()
-                                                            .map(|elem| {
-                                                                elem.class("hidden flex flex-col items-center")
-                                                            });
-                                                    }
-                                                >
-                                                    {nickname}
-                                                </button>
-                                            }
-                                        })
-                                        .collect_view()
-                                }}
-                            </div>
-                            {children.as_ref().map(|children| children())}
-                        </div>
-                    </div>
+                                    true
+                                },
+                                None => true,
+                            }
+                        })
+                        .map(|elem| elem.nickname.clone())
+                        .take(MAX_USER_DISPLAY_LIMIT)
+                        .collect()
+                });
+
+                view! {
+                    <Field label=title>
+                        <AutoComplete
+                            value=input
+                        >
+                            <For
+                                each=move || options.get()
+                                key=|option| option.clone()
+                                let:option
+                            >
+
+                                <AutoCompleteOption value=option
+                                    .clone()>{option}</AutoCompleteOption>
+                            </For>
+
+                        </AutoComplete>
+                    </Field>
+                    {children.as_ref().map(|children| children())}
                 }
                     .into_any()
             }}
--- src/frontend/route/split_cost.rs
+++ src/frontend/route/split_cost.rs
@@ -1,11 +1,16 @@
 use leptos::{prelude::*, task::spawn_local};
-use thaw::{Flex, FlexAlign, Grid, GridItem};
+use thaw::{
+    Button, ButtonAppearance, ButtonSize, Field, FieldOrientation, Flex, FlexAlign, Grid, GridItem,
+    Input,
+};
 
 use crate::{
     backend::core::behaviour::split_cost::split_cost,
-    frontend::component::{
-        error_message::ErrorMessage, multi_user_selection::SelectMultiUser,
-        single_user_selection::SelectSingleUser,
+    frontend::{
+        component::{
+            multi_user_selection::SelectMultiUser, single_user_selection::SelectSingleUser,
+        },
+        shared::throw_error_none_view,
     },
 };
 
@@ -38,56 +43,44 @@ pub fn Show() -> impl IntoView {
             let msg = error_signal.get();
             match msg.len() {
                 0 => ().into_any(),
-                _ => view! { <ErrorMessage error=msg /> }.into_any(),
+                _ => {
+                    throw_error_none_view(msg)
+                },
             }
         }}
         <Flex vertical=true align=FlexAlign::Center>
             <Grid cols=2 x_gap=10 y_gap=10>
                 <GridItem>
-                    <SelectSingleUser
-                        title=String::from("Who are you?")
-                        input=primary_user
-                        extra_class="w-full h-full".to_string()
-                    />
-                </GridItem>
-                <GridItem>
-                    <SelectMultiUser
-                        title=String::from("Who do you want to split the cost with?")
-                        users_input=secondary_users
-                        single_user_extra_class="w-full h-full".to_string()
-                    />
-                </GridItem>
-                <GridItem>
-                    <div class="flex justify-center items-center p-2 bg-indigo-100 rounded">
-                        <div class="flex flex-col items-center gap-3">
-                            <a class="text-indigo-700 w-fit h-fit">"How much?"</a>
-                            <a class="text-indigo-700 w-fit h-fit">"Description:"</a>
-                        </div>
-                        <div class="flex flex-col items-center gap-3">
-                            <input
-                                class="ml-5 text-black rounded-[5px] text-center text-indigo-700"
-                                type="text"
-                                value="0,00"
-                                bind:value=money_input
-                            />
-                            <input
-                                class="ml-5 text-black rounded-[5px] text-center text-indigo-700"
-                                type="text"
-                                value=""
-                                bind:value=description_input
-                            />
-                        </div>
+                    <div class="pt-4">
+                        <SelectSingleUser
+                            title=String::from("Who are you?")
+                            input=primary_user
+                        />
                     </div>
                 </GridItem>
                 <GridItem>
-                    <div class="flex items-center justify-center bg-indigo-100 rounded p-2">
-                        <button
-                            class="w-full bg-indigo-700 hover:bg-pink-700 text-white p-3 rounded"
-                            on:click=on_click
-                        >
-                            "Split cost"
-                        </button>
+                    <div class="pt-4">
+                        <SelectMultiUser
+                            title=String::from("Who do you want to split the cost with?")
+                            users_input=secondary_users
+                        />
                     </div>
+                </GridItem>
+                <GridItem>
+                    <Field label="How much?" orientation=FieldOrientation::Horizontal required=true>
+                        <Input value=money_input/>
+                    </Field>
+                    <Field label="Description" orientation=FieldOrientation::Horizontal>
+                        <Input value=description_input/>
+                    </Field>
+                </GridItem>
+                <GridItem>
+                    <Button
+                        class="w-full"
+                        appearance=ButtonAppearance::Primary
+                        size=ButtonSize::Large
+                        on_click=on_click
+                    >"Split cost"</Button>
                 </GridItem>
             </Grid>
         </Flex>
