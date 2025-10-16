cb49c0f27a99742fa2905f42168a83b1c0f31d4c
DestinyofYeet <olebend@gmail.com>
2025-10-16 15:14:27 +0200

    Moves create article to thaw ui

--- src/frontend/route/articles/create.rs
+++ src/frontend/route/articles/create.rs
@@ -1,51 +1,54 @@
-use leptos::prelude::*;
-
-use crate::{
-    backend::core::behaviour::article_new::CreateArticle, frontend::shared::throw_error_none_view,
+use leptos::{ev, prelude::*};
+use thaw::{
+    Button, ButtonAppearance, ButtonSize, ButtonType, Field, FieldContextInjection,
+    FieldContextProvider, Flex, FlexJustify, Input, InputRule,
 };
+
+use crate::{backend::core::behaviour::article_new::CreateArticle, frontend::shared::throw_error};
 
 #[component]
 pub fn Create() -> impl IntoView {
     let create_article_action = ServerAction::<CreateArticle>::new();
+    Effect::new(move || {
+        if let Some(Err(e)) = create_article_action.value().get() {
+            throw_error(e);
+        }
+    });
     view! {
-        <div class="flex h-screen bg-gray-900">
-            <div class="w-full max-w-xs m-auto bg-indigo-100 rounded p-5">
-                <ActionForm action=create_article_action>
-                    <div>
-                        <label class="block mb-2 text-indigo-500">"Name: "</label>
-                        <input
-                            class="w-full p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300"
-                            type="text"
-                            name="name"
-                        />
-                        <label class="block mb-2 text-indigo-500">"Cost: "</label>
-                        <input
-                            class="w-full p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300"
-                            type="text"
-                            name="cost"
-                        />
-                    </div>
-                    <div>
-                        <input
-                            class="w-full bg-indigo-700 hover:bg-pink-700 text-white font-bold py-2 px-4 mb-6 rounded"
-                            type="submit"
-                            value="Create Article"
-                        />
-                    </div>
-                </ActionForm>
-                <div>
-                    {move || match create_article_action.value().get() {
-                        Some(Err(e)) => {
-                            let msg = match e {
-                                ServerFnError::ServerError(msg) => msg,
-                                _ => e.to_string(),
-                            };
-                            throw_error_none_view(format!("Failed to create article: {msg}"))
-                        }
-                        _ => ().into_any(),
-                    }}
-                </div>
-            </div>
-        </div>
+        <Flex justify=FlexJustify::Center>
+            <ActionForm action=create_article_action>
+                <FieldContextProvider>
+                    <Flex vertical=true justify=FlexJustify::SpaceEvenly>
+                        <Field label="Name" name="name" required=true>
+                            <Input
+                                rules=vec![InputRule::required(true.into())]
+                                autocomplete="off"
+                            />
+                        </Field>
+                        <Field label="Cost" name="cost" required=true>
+                            <Input
+                                rules=vec![InputRule::required(true.into())]
+                                autocomplete="off"
+                            />
+                        </Field>
+                        <Button
+                            button_type=ButtonType::Submit
+                            appearance=ButtonAppearance::Primary
+                            size=ButtonSize::Medium
+                            on_click={
+                                let field_context = FieldContextInjection::expect_context();
+                                move |e: ev::MouseEvent| {
+                                    if !field_context.validate() {
+                                        e.prevent_default();
+                                    }
+                                }
+                            }
+                        >
+                            "Create Article"
+                        </Button>
+                    </Flex>
+                </FieldContextProvider>
+            </ActionForm>
+        </Flex>
     }
 }
