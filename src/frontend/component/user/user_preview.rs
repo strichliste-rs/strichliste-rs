6c565005e3d8c3f33e60fa3e2ee58460c73a89c7
DestinyofYeet <olebend@gmail.com>
2025-10-16 06:44:40 +0200

    Aligns User boxes and money on that user boxes in main view

--- src/frontend/component/user/user_preview.rs
+++ src/frontend/component/user/user_preview.rs
@@ -5,10 +5,10 @@ use crate::{backend::core::User, model::Money};
 #[component]
 pub fn UserPreview(user: User) -> impl IntoView {
     view! {
-        <div class="flex flex-col bg-[#2e3d4d] gap-2 rounded-[10px] py-2">
-            <p class="text-center text-white">{user.nickname.clone()}</p>
+        <div class="flex flex-col justify-around bg-[#2e3d4d] gap-2 rounded-[10px] py-2 h-full">
+            <p class="text-center text-white pt-1 pl-1 pr-1 h-[70%]">{user.nickname.clone()}</p>
             <p
-                class="text-center"
+                class="text-center h-[30%]"
                 class=("text-red-500", move || { user.money.value < 0 })
                 class=("text-green-500", move || { user.money.value >= 0 })
             >
