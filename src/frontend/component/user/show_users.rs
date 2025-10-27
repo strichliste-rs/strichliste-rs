a7fa9dcbbee3401d3ca8ed61e621080ef8237201
cncptpr <concept1928@gmail.com>
2025-10-27 21:10:13 +0100

    feat: added prefix filter

--- src/frontend/component/user/show_users.rs
+++ src/frontend/component/user/show_users.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use leptos::prelude::*;
 use thaw::Spinner;
 
@@ -7,11 +8,12 @@ use crate::frontend::{
         caching_layer::CachingLayer,
         frontend_store::{FrontendStoreStoreFields, FrontendStoreType},
     },
+    route::home::PREFIX_FILTER_NON_ALPHABETIC_VALUE,
     shared::throw_error,
 };
 
 #[component]
-pub fn ShowUsers() -> impl IntoView {
+pub fn ShowUsers(prefix_filter: Signal<Option<char>>) -> impl IntoView {
     let store = expect_context::<FrontendStoreType>();
 
     let cache = store.cachinglayer().get_untracked();
@@ -29,6 +31,33 @@ pub fn ShowUsers() -> impl IntoView {
             if users.read().is_fetching.get() && users.read().value.get().is_empty() {
                 return view! { <Spinner label="Loading users!" /> }.into_any();
             }
+            let filterd_users = match prefix_filter.get() {
+                Some(filter) => {
+                    users
+                        .read()
+                        .value
+                        .get()
+                        .into_iter()
+                        .filter_map(|user| {
+                            let first_letter = user
+                                .nickname
+                                .chars()
+                                .nth(0)
+                                .expect("nickname isn't allowed empty")
+                                .to_ascii_lowercase();
+                            if filter == PREFIX_FILTER_NON_ALPHABETIC_VALUE {
+                                if first_letter.is_alphabetic() {
+                                    return None;
+                                }
+                            } else if !(first_letter == filter) {
+                                return None;
+                            }
+                            Some(user)
+                        })
+                        .collect_vec()
+                }
+                None => users.read().value.get(),
+            };
 
             view! {
                 <div class="grid">
@@ -37,19 +66,18 @@ pub fn ShowUsers() -> impl IntoView {
                         class="grid gap-5"
                         style="grid-template-columns: repeat(auto-fill, minmax(8rem, 1fr));"
                     >
-                        {users
-                            .read()
-                            .value
-                            .get()
+                        {filterd_users
                             .into_iter()
                             .map(|user| {
                                 let id = user.id;
+                                Some(
 
-                                view! {
-                                    <a href=format!("/user/{}", id.0)>
-                                        <UserPreview user />
-                                    </a>
-                                }
+                                    view! {
+                                        <a href=format!("/user/{}", id.0)>
+                                            <UserPreview user />
+                                        </a>
+                                    },
+                                )
                             })
                             .collect_view()
                             .into_any()}
