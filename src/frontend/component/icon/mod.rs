ece63e877275bf71f0836fb54e205d6a755ab6e5
DestinyofYeet <olebend@gmail.com>
2025-11-05 15:34:09 +0100

    chore(svg): Moves svg to component

--- src/frontend/component/icon/mod.rs
+++ src/frontend/component/icon/mod.rs
@@ -3,6 +3,7 @@ pub mod arrow_left_right;
 pub mod arrow_right;
 pub mod basket;
 pub mod delete_button;
+pub mod send_money;
 pub mod settings_icon;
 
 pub use arrow_left::*;
@@ -10,4 +11,5 @@ pub use arrow_left_right::*;
 pub use arrow_right::*;
 pub use basket::*;
 pub use delete_button::*;
+pub use send_money::*;
 pub use settings_icon::*;
